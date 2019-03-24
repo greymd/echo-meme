57cd82ceea64b5e6c56466653e32de814b457056
greymd <greengregson@gmail.com>
2019-03-24 23:32:52 +0000

    Add package generator

---
+++ genpkg.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -ue
+trap 'rm -f $THIS_DIR/echo-meme.tar.gz' EXIT
+readonly THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]:-${(%):-%N}}")" && pwd)"
+EM_VERSION="$(/bin/bash "${THIS_DIR}"/bin/echo-meme --version | awk '{print $2}' | sed 's/^v//')"
+tar zcvf "${THIS_DIR}"/echo-meme.tar.gz -C "${THIS_DIR}" bin man .tar2package.yml
+docker run -i greymd/tar2rpm < "${THIS_DIR}"/echo-meme.tar.gz > "${THIS_DIR}/echo-meme-${EM_VERSION}.rpm"
+docker run -i greymd/tar2deb < "${THIS_DIR}"/echo-meme.tar.gz > "${THIS_DIR}/echo-meme-${EM_VERSION}.deb"
