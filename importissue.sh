dcfc18fcbeb14a19b21bf500914c23858e97e524
greymd <greengregson@gmail.com>
2019-04-06 17:42:43 +0100

    update importissue.sh

--- importissue.sh
+++ importissue.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 ## Require jq and GNU sed.
+debug="${1-}"
+cmd='sed -f- -i bin/echo-meme'
+[[ "$debug" == "--debug" ]] && cmd='cat'
+
 curl --retry 3 -L "https://api.github.com/repos/greymd/echo-meme/issues" \
   | jq -r '.[].body' \
-  | grep -E 'template:\[.+\]' \
+  | grep -oE 'template:\[.+\]' \
   | sed -r 's/^template:\[(.+)\]$/\1/' \
   | sed 's/"/\\"/' \
   | sed 's|.*|/^[^#]*"@@@TEMPLATE_INSERT_HERE@@@"/i \\ \\ "&"|' \
-  | sed -f- -i bin/echo-meme
+  | eval "$cmd"
